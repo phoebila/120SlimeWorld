99989d1f79a45d9453fea8b8241bf1244533a00b
Phoebe <128556139+phoebila@users.noreply.github.com>
2024-02-16 15:39:59 -0800

    base files

---
+++ src/Overworld.js
@@ -0,0 +1,89 @@
+class Overworld extends Phaser.Scene {
+    constructor() {
+        super('overworldScene')
+    }
+
+    init() {
+        this.VEL = 100  // slime velocity constant
+    }
+
+    preload() {
+        this.load.path = './assets/'
+        this.load.spritesheet('slime', 'slime.png', {
+            frameWidth: 16,
+            frameHeight: 16
+        })
+
+        this.load.image('tilesetImage', 'tileset.png')
+
+        this.load.tilemapTiledJSON('tilemapJSON', 'overworld.json')
+    }
+
+    create() {
+
+        //tilemaps
+        const map = this.add.tilemap('tilemapJSON')
+        //attach image to data
+        const tileset = map.addTilesetImage('tileset', 'tilesetImage')
+        //layers
+        const bgLayer = map.createLayer('BG', tileset, 0, 0)
+
+        const terrainLayer = map.createLayer('Terrain', tileset, 0, 0)
+        terrainLayer.setCollisionByProperty({
+            collides: true
+        })
+        const treesLayer = map.createLayer('Trees', tileset, 0, 0)
+        treesLayer.setCollisionByProperty({
+            collides: true
+        })
+
+        const slimeSpawn = map.findObject("Spawns", (obj) => obj.name === 'slimeSpawn')
+
+        // add slime
+        this.slime = this.physics.add.sprite(slimeSpawn.x, slimeSpawn.y, 'slime', 0)
+        this.slime.body.setCollideWorldBounds(true)
+
+        // slime animation
+        this.anims.create({
+            key: 'jiggle',
+            frameRate: 6, 
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('slime', {
+                start: 0,
+                end: 1
+            })
+        })
+        this.slime.play('jiggle')
+
+        // cams
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
+        this.cameras.main.startFollow(this.slime, true, .25, .25, )
+        this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
+
+        this.physics.add.collider(this.slime, terrainLayer)
+        this.physics.add.collider(this.slime, treesLayer)
+
+
+        // input
+        this.cursors = this.input.keyboard.createCursorKeys()
+    }
+
+    update() {
+        // slime movement
+        this.direction = new Phaser.Math.Vector2(0)
+        if(this.cursors.left.isDown) {
+            this.direction.x = -1
+        } else if(this.cursors.right.isDown) {
+            this.direction.x = 1
+        }
+
+        if(this.cursors.up.isDown) {
+            this.direction.y = -1
+        } else if(this.cursors.down.isDown) {
+            this.direction.y = 1
+        }
+
+        this.direction.normalize()
+        this.slime.setVelocity(this.VEL * this.direction.x, this.VEL * this.direction.y)
+    }
+}
